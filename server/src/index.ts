99eb863e2ccac8be6bf832206e3c01cc7c1b661b
Abhishek Dash <abhishek.dash@rustoleum.com>
2026-04-02 11:58:54 +0530

    Add gitignore

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+
+async function startServer() {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(4000, () => {
+    console.log("Server running at http://localhost:4000/graphql");
+  });
+}
+
+startServer();
